4ef5ca193ca2f4116e05b15bfd7353865704f73b
gempellm <gempellm@mail.ru>
2022-03-13 21:34:43 +0300

    Added security library, established mysql remote connection

--- src/main/java/com/cryptolisting/springreactjs/controller/APIController.java
+++ src/main/java/com/cryptolisting/springreactjs/controller/APIController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexController {
+public class APIController {
 
     @GetMapping("")
     public ModelAndView home() {
