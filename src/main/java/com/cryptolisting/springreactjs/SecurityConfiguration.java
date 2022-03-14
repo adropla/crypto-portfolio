fe5e88f5e89a1603b8e18840effa8eff5cb52a7c
gempellm <gempellm@mail.ru>
2022-03-14 17:40:21 +0300

    Added JWT encoding and decoding

--- src/main/java/com/cryptolisting/springreactjs/SecurityConfiguration.java
+++ src/main/java/com/cryptolisting/springreactjs/SecurityConfiguration.java
@@ -1,22 +1,29 @@
 package com.cryptolisting.springreactjs;
 
+import com.cryptolisting.springreactjs.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    private SecurityUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,12 +32,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .antMatchers("/exchanges").hasRole("USER")
-                .antMatchers("/portfolio").hasRole("USER")
-                .antMatchers("/watchlist").hasRole("USER")
-                .antMatchers("/").permitAll()
-                .and().formLogin();
+        http.csrf().disable()
+                .authorizeRequests().antMatchers( "/", "/*.html", "/authenticate").permitAll().
+                antMatchers("/test").authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
