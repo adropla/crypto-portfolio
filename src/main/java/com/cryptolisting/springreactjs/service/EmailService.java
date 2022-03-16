9e1e5a85b46aa969db734cde929bb26d7f4a3ff7
gempellm <gempellm@mail.ru>
2022-03-16 15:26:27 +0300

    Full cycle of registration is complete. Email sending of confirmation links is done.

---
+++ src/main/java/com/cryptolisting/springreactjs/service/EmailService.java
@@ -0,0 +1,42 @@
+package com.cryptolisting.springreactjs.service;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.*;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
+@Service
+public class EmailService implements EmailSender {
+
+    private final static Logger LOGGER = LoggerFactory
+            .getLogger(EmailService.class);
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Email Confirmation");
+            helper.setFrom("tonsoflions@gmail.com");
+            mailSender.send(mimeMessage);
+
+        } catch (MessagingException ex) {
+            LOGGER.error("failed to send email", ex);
+            throw new IllegalStateException("failed to send email");
+        }
+    }
+}
