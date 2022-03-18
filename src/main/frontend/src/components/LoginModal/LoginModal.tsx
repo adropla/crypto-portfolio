f8011edac5bf1fd7c7f8d9382240fdac2789a804
Alexandr <sao.bur@yandex.ru>
2022-03-18 15:00:59 +0300

    login modal ready

--- src/main/frontend/src/components/LoginModal/LoginModal.tsx
+++ src/main/frontend/src/components/LoginModal/LoginModal.tsx
@@ -1,10 +1,20 @@
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
+import {
+    EyeInvisibleOutlined,
+    EyeTwoTone,
+    LockOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import RoundModal from '../../styledComponents/RoundModal';
 import { ModalProps } from '../../types/ModalProps';
 
 import styles from './LoginModal.module.scss';
+
+const { Text } = Typography;
+
+const inputPasswordIconRender = (passVisible: boolean) =>
+    passVisible ? <EyeTwoTone /> : <EyeInvisibleOutlined />;
 
 const LoginModal = ({ visible, toogleModal }: ModalProps) => {
     const handleOk = () => {
@@ -32,6 +42,23 @@ const LoginModal = ({ visible, toogleModal }: ModalProps) => {
                 initialValues={{ remember: true }}
                 onFinish={onFinish}
             >
+                <Form.Item noStyle>
+                    <Text style={{ fontSize: '25px', fontWeight: 'bold' }}>
+                        Login
+                    </Text>
+                </Form.Item>
+                <Form.Item noStyle>
+                    <Form.Item>
+                        <Text style={{ fontSize: '14px', color: 'darkgray' }}>
+                            Don&apos;t have a account?
+                        </Text>
+                        <Link to="/" style={{ fontSize: '14px' }}>
+                            {' '}
+                            Register now!
+                        </Link>
+                    </Form.Item>
+                </Form.Item>
+
                 <Form.Item
                     name="email"
                     rules={[
@@ -41,12 +68,7 @@ const LoginModal = ({ visible, toogleModal }: ModalProps) => {
                         },
                     ]}
                 >
-                    <Input
-                        prefix={
-                            <UserOutlined className="site-form-item-icon" />
-                        }
-                        placeholder="Email"
-                    />
+                    <Input prefix={<UserOutlined />} placeholder="Email" />
                 </Form.Item>
                 <Form.Item
                     name="password"
@@ -57,35 +79,30 @@ const LoginModal = ({ visible, toogleModal }: ModalProps) => {
                         },
                     ]}
                 >
-                    <Input
-                        prefix={
-                            <LockOutlined className="site-form-item-icon" />
-                        }
+                    <Input.Password
+                        prefix={<LockOutlined />}
                         type="password"
                         placeholder="Password"
+                        iconRender={inputPasswordIconRender}
                     />
                 </Form.Item>
                 <Form.Item name="remember" valuePropName="checked">
-                    <Checkbox>Remember me</Checkbox>
+                    <div className={styles.flexWrapper}>
+                        <Link className={styles.forgotLink} to="/">
+                            Forgot password?
+                        </Link>
+                        <Checkbox>Remember me</Checkbox>
+                    </div>
                 </Form.Item>
 
-                <Form.Item>
-                    <Link className="login-form-forgot" to="/">
-                        Forgot password
-                    </Link>
-                </Form.Item>
-
-                <Form.Item>
+                <Form.Item noStyle>
                     <Button
                         type="primary"
                         htmlType="submit"
-                        className="login-form-button"
+                        className={styles.btn}
                     >
                         Log in
                     </Button>
-                </Form.Item>
-                <Form.Item>
-                    Or <Link to="/">register now!</Link>
                 </Form.Item>
             </Form>
         </RoundModal>
